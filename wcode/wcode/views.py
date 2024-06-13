d54fcc32128b66d34580e43a7406883a94fe7215
irwin-rex <irwin.a@equipohealth.com>
2024-06-13 19:16:03 +0530

    updated a mini django project

---
+++ wcode/wcode/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+def health_check(request):
+    # You can add additional checks here if needed
+    status = {
+        'status': 'ok',
+        'message': 'Application is healthy'
+    }
+    return JsonResponse(status)
