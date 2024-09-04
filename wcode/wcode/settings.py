cc1dd35c017d79707910179bb33dba2d86ae1596
irwin-rex <irwin.a@equipohealth.com>
2024-09-04 18:46:45 +0530

    removed the print statement in settings.py

--- wcode/wcode/settings.py
+++ wcode/wcode/settings.py
@@ -124,8 +124,6 @@ def get_messages(request, chat_id):
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
-print('hello')
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
