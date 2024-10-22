2da6c1ccd29496d4e666af6ea7f85ad28b271abc
Irwin-rex <irwin.a@equipohealth.com>
2024-10-22 18:57:17 +0530

    New Beginning GOoooooooo

--- wcode/chat/urls.py
+++
@@ -1,9 +0,0 @@
-# chat/urls.py
-
-from django.urls import path
-from .views import authenticate_and_create_chatroom
-
-urlpatterns = [
-    # Endpoint for creating a chatroom with authentication via JWT
-    path('create-chatroom/', authenticate_and_create_chatroom, name='create-chatroom'),
-]
--- wcode/wcode/routing.py
+++
@@ -1,9 +0,0 @@
-# myproject/routing.py
-
-from django.urls import re_path
-from chat.consumers import ChatConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/chat/(?P<room_name>[^/]+)/$', ChatConsumer.as_asgi()),  # Adjust the regex pattern as needed
-]
-
